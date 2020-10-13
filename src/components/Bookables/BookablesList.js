e0c322bd12c5de87b15edc7a1768c94e2fb9aad5
John Larsen <jr.larsen@mac.com>
2020-10-13 14:46:38 +0100

    use a standard variable to hold the index of the selected bookable

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -2,22 +2,26 @@ import React from 'react';
 import {bookables} from "../../static.json";
 
 export default function BookablesList () {
-
   const group = "Rooms";
-
   const bookablesInGroup = bookables.filter(b => b.group === group);
 
-  const bookableIndex = 1;
+  let bookableIndex = 1;
+
+  function changeBookable (selectedIndex) {
+    bookableIndex = selectedIndex;
+    console.log(selectedIndex);
+  }
 
   return (
     <ul className="bookables items-list-nav">
       {bookablesInGroup.map((b, i) => (
         <li
-          key={b.title}
+          key={b.id}
           className={i === bookableIndex ? "selected" : null}
         >
           <button
             className="btn"
+            onClick={() => changeBookable(i)}
           >
             {b.title}
           </button>
