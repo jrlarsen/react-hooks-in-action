09382d2ab642975296644288c652b05b1c9b75ea
John Larsen <jr.larsen@mac.com>
2020-10-13 16:40:59 +0100

    display details for the selected user

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,24 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 import {users} from "../../static.json";
 
 export default function UsersList () {
   const [userIndex, setUserIndex] = useState(0);
+  const user = users[userIndex];
 
   return (
-    <ul className="users items-list-nav">
-      {users.map((u, i) => (
-        <li
-          key={u.id}
-          className={i === userIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setUserIndex(i)}
+    <Fragment>
+      <ul className="users items-list-nav">
+        {users.map((u, i) => (
+          <li
+            key={u.id}
+            className={i === userIndex ? "selected" : null}
           >
-            {u.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+            <button
+              className="btn"
+              onClick={() => setUserIndex(i)}
+            >
+              {u.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
