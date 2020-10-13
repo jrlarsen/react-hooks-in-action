46ad81e55e887e4ac6c593317145bda14456a379
John Larsen <jr.larsen@mac.com>
2020-10-13 16:06:06 +0100

    add a Next button to cycle through the bookables

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,26 +1,43 @@
 import React, {useState} from 'react';
 import {bookables} from "../../static.json";
+import {FaArrowRight} from "react-icons/fa";
 
 export default function BookablesList () {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter(b => b.group === group);
   const [bookableIndex, setBookableIndex] = useState(1);
 
+  function nextBookable () {
+    setBookableIndex(i => (i + 1) % bookablesInGroup.length);
+  }
+
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li
-          key={b.id}
-          className={i === bookableIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setBookableIndex(i)}
+    <div>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li
+            key={b.id}
+            className={i === bookableIndex ? "selected" : null}
           >
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+            <button
+              className="btn"
+              onClick={() => setBookableIndex(i)}
+            >
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button
+          className="btn"
+          onClick={nextBookable}
+          autoFocus
+        >
+          <FaArrowRight/>
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 }
