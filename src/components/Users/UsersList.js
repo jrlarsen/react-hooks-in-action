16c46ce0059f6f96493075d0557a4bdccc94e567
John Larsen <jr.larsen@mac.com>
2020-10-15 13:12:30 +0100

    handle isLoading and error states in UsersList

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,21 +1,40 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import Spinner from "../UI/Spinner";
+import getData from "../../utils/api"; // we'll use this api function
 
 export default function UsersList () {
+  // include state for an error object and an isLoading flag
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   const [users, setUsers] = useState(null);
   const [userIndex, setUserIndex] = useState(0);
   const user = users?.[userIndex];
 
+  // update the effect to use the getData function
   useEffect(() => {
-    fetch("http://localhost:3001/users")
-      .then(resp => resp.json())
-      .then(data => setUsers(data));
+    getData("http://localhost:3001/users")
+      .then(data => {
+        setUsers(data);
+        setIsLoading(false); // the data has finished loading
+      })
+      .catch(error => {
+        setError(error); // set the error object
+        setIsLoading(false); // we're no longer loading
+      });
   }, []);
 
-  if (users === null) {
+  // alternative UI for when there's an error
+  if (error) {
+    return <p>{error.message}</p>
+  }
+
+  // alternative UI while users load
+  if (isLoading) {
     return <p><Spinner/> Loading users...</p>
   }
 
+  // this UI is unchanged
   return (
     <Fragment>
       <ul className="users items-list-nav">
