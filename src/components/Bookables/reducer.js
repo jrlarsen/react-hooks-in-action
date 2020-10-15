9745cd794fe9ec89ff7290b086919a50da1ed055
John Larsen <jr.larsen@mac.com>
2020-10-15 12:39:19 +0100

    update reducer to handle fetch states

--- src/components/Bookables/reducer.js
+++ src/components/Bookables/reducer.js
@@ -1,6 +1,5 @@
 export default function reducer (state, action) {
   switch (action.type) {
-
     case "SET_GROUP":
       return {
         ...state,
@@ -21,13 +20,32 @@ export default function reducer (state, action) {
       };
 
     case "NEXT_BOOKABLE":
-      const count = state.bookables.filter(
-        b => b.group === state.group
-      ).length;
-
+      const count = state.bookables.filter(b => b.group === state.group).length;
       return {
         ...state,
         bookableIndex: (state.bookableIndex + 1) % count
+      };
+
+    case "FETCH_BOOKABLES_REQUEST":
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+        bookables: []
+      };
+
+    case "FETCH_BOOKABLES_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        bookables: action.payload
+      };
+
+    case "FETCH_BOOKABLES_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
       };
 
     default:
