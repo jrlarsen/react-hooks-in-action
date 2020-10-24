18b5576e8b201d6f48ad3150380976bf10b8fd2b
John Larsen <jr.larsen@mac.com>
2020-10-24 14:45:11 +0100

    no props needed on user picker

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,8 @@ export default function App () {
               </ul>
             </nav>
 
-            <UserPicker user={user} setUser={setUser}/>
+            {/* no props needed on UserPicker */}
+            <UserPicker/>
           </header>
 
           <Routes>
