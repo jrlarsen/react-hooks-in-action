f781892853c9ab1ca49bbbd9f085a46133eec38b
John Larsen <jr.larsen@mac.com>
2020-10-25 18:11:38 +0000

    slide the booking grid across with React Spring

--- src/components/Bookings/BookingsGridSlide.js
+++ src/components/Bookings/BookingsGridSlide.js
@@ -1,22 +1,14 @@
 import React from "react";
-import {useTransition, animated} from "react-spring";
+import {animated} from "react-spring";
 
-import {shortISO} from "../../utils/date-wrangler";
+import {useSlide} from "./bookingsHooks";
 
 import BookingsGrid from "./BookingsGrid";
 
 export default function BookingsGridSlide(props) {
   const {week, bookable, booking, setBooking} = props;
 
-  const transitions = useTransition(
-    {bookable, week},
-    item => `${item.bookable.id}_${shortISO(item.week.start)}`,
-    {
-      from:  {opacity: 1, transform: 'translate3d(0, -100%, 0)'},
-      enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
-      leave: {opacity: 0, transform: 'translate3d(0, 20%, 0)'}
-    }
-  );
+  const transitions = useSlide(bookable, week);
 
   return (
     <div className="grid-wrapper">
--- src/components/Bookings/bookingsHooks.js
+++ src/components/Bookings/bookingsHooks.js
@@ -1,6 +1,7 @@
-import {useMemo} from "react";
+import {useMemo, useEffect, useRef} from "react";
 import {useSearchParams} from "react-router-dom";
 import {useQuery, useMutation, queryCache} from "react-query";
+import {useTransition} from "react-spring";
 
 import {shortISO, isDate} from "../../utils/date-wrangler";
 import getData, {createItem, editItem, deleteItem} from "../../utils/api";
@@ -102,4 +103,38 @@ export function useDeleteBooking (key) {
       }
     }
   )
+}
+
+function getSlideStyles(date1, date2) {
+  // vertical transition
+  if (date1 === date2) {
+    return {
+      from: {opacity: 1, transform: "translate3d(0, -100%, 0)"},
+      enter: {opacity: 1, transform: "translate3d(0, 0, 0)"},
+      leave: {opacity: 0, transform: "translate3d(0, 20%, 0)"}
+    }
+  }
+
+  // horizontal transition
+  const percent = date1 < date2 ? 100 : -100;
+  return {
+    from: {opacity: 1, transform: `translate3d(${percent}%, 0, 0)`},
+    enter: {opacity: 1, transform: "translate3d(0, 0, 0)"},
+    leave: {opacity: 0, transform: `translate3d(${-percent}%, 0, 0)`}
+  };
+}
+
+export function useSlide (bookable, week) {
+  const weekStart = shortISO(week.start);
+  const weekRef = useRef(weekStart);
+
+  useEffect(() => {
+    weekRef.current = weekStart;
+  }, [weekStart]);
+
+  return useTransition(
+    {bookable, week},
+    item => `${item.bookable.id}_${shortISO(item.week.start)}`,
+    getSlideStyles(weekRef.current, weekStart)
+  );
 }
