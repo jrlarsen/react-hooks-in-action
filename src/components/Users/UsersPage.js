37548ada5573e068e794048605f9d0b5f92e9fdc
John Larsen <jr.larsen@mac.com>
2020-10-26 17:09:05 +0000

    prefetch user data and image

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,4 +1,6 @@
 import React, {useState, Suspense} from "react";
+import {queryCache} from "react-query";
+import getData from "../../utils/api";
 import UsersList from "./UsersList";
 import {useUser} from "./UserContext";
 import PageSpinner from "../UI/PageSpinner";
@@ -11,6 +13,20 @@ export default function UsersPage() {
 
   function switchUser(nextUser) {
     setSelectedUser(nextUser);
+
+    queryCache.prefetchQuery(
+      ["user", nextUser.id],
+      () => getData(`http://localhost:3001/users/${nextUser.id}`)
+    );
+
+    queryCache.prefetchQuery(
+      `http://localhost:3001/img/${nextUser.img}`,
+      () => new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.src = `http://localhost:3001/img/${nextUser.img}`;
+      })
+    );
   }
 
   return user ? (
