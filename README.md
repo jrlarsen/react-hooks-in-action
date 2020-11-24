67e47c9188bf20e0ad9ed3a04c18725c73729cb9
John Larsen <git@room51.co.uk>
2020-11-24 18:29:00 +0000

    update to React 17

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# react-hooks-in-action
-
-Code for the book [React Hooks in Action](https://www.manning.com/books/react-hooks-in-action?a_aid=r51&a_bid=b49082e3&chan=gh) by John Larsen.
-
-* The code is for the book's Booking App example.
-* The repo is broken into branches.
-* Each branch corresponds to a progression in the book.
-* E.g. 304-next-button is the 4th example in chapter 3.
+# React Hooks in Action
--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./App.css";
-import Bookables from "./components/Bookables.js";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <Bookables />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
