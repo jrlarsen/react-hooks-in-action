f002cfdf61f57158f3627213a18579797cb41f9d
John Larsen <git@room51.co.uk>
2020-11-25 17:10:49 +0000

    update to React 17

--- src/components/Bookings/BookingsPage.js
+++ src/components/Bookings/BookingsPage.js
@@ -1,7 +1,6 @@
-import React from "react";
 import WeekPicker from "./WeekPicker";
 
-export default function BookingsPage() {
+export default function BookingsPage () {
   return (
     <main className="bookings-page">
       <p>Bookings!</p>
