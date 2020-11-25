bd3c96cd4a1544b533af2fb02d78d1715e0b3b84
John Larsen <git@room51.co.uk>
2020-11-25 16:18:17 +0000

    update to React 17

--- src/components/Bookings/WeekPicker.js
+++ src/components/Bookings/WeekPicker.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from "react";
+import {useReducer} from "react";
 import reducer from "./weekReducer";
 import {getWeek} from "../../utils/date-wrangler";
 import {FaChevronLeft, FaCalendarDay, FaChevronRight} from "react-icons/fa";
