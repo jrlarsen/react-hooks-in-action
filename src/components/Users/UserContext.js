21e0971444a68a9eea2615c4cb18a71fe32a077a
John Larsen <jr.larsen@mac.com>
2020-12-07 14:34:58 +0000

    update to React 17

--- src/components/Users/UserContext.js
+++ src/components/Users/UserContext.js
@@ -1,4 +1,4 @@
-import React, {createContext, useContext, useState} from "react";
+import {createContext, useContext, useState} from "react";
 
 const UserContext = createContext();
 const UserSetContext = createContext();
