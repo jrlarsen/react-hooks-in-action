000b9e652c35dd8cd06a30801ed3691d03e452e7
John Larsen <jr.larsen@mac.com>
2020-12-08 17:05:56 +0000

    use unstable_createRoot for Concurrent Mode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+ReactDOM
+  .unstable_createRoot(root)
+  .render(<App />);
