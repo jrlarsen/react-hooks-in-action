08e4214e8089ca67c5faeed978e1c237d87be6f7
John Larsen <git@room51.co.uk>
2021-01-07 17:48:10 +0000

    update React Query to v3

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -3,17 +3,20 @@ import {
   unstable_useDeferredValue as useDeferredValue,
   Suspense
 } from "react";
-import {queryCache} from "react-query";
-import getData from "../../utils/api";
+
 import UsersList from "./UsersList";
 import {useUser} from "./UserContext";
 import PageSpinner from "../UI/PageSpinner";
 import UserDetails from "./UserDetails";
 
+import {useQueryClient} from "react-query";
+import getData from "../../utils/api";
+
 export default function UsersPage () {
   const [loggedInUser] = useUser();
   const [selectedUser, setSelectedUser] = useState(null);
   const user = selectedUser || loggedInUser;
+  const queryClient = useQueryClient();
 
   const deferredUser = useDeferredValue(user) || user;
 
@@ -22,12 +25,12 @@ export default function UsersPage () {
   function switchUser (nextUser) {
     setSelectedUser(nextUser);
 
-    queryCache.prefetchQuery(
+    queryClient.prefetchQuery(
       ["user", nextUser.id],
       () => getData(`http://localhost:3001/users/${nextUser.id}`)
     );
 
-    queryCache.prefetchQuery(
+    queryClient.prefetchQuery(
       `http://localhost:3001/img/${nextUser.img}`,
       () => new Promise((resolve) => {
         const img = new Image();
